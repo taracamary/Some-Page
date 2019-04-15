814d8d3c8902b1c0933329817acef30acf254f11
Sergey Kraevskiy <skraevsky@gmail.com>
2019-04-15 11:23:53 +0300

    Fix .videos__wrapper container

--- src/ServicesPage.js
+++ src/ServicesPage.js
@@ -27,7 +27,7 @@ constructor(props){
 
 
         <div className="videos__wrapper">
-        <div className="videos__item">
+          <div className="videos__item">
             <div className="imgBx">
               <img src={PUBLIC_URL + '/images/8.png'} alt="thumb" />
             </div>
@@ -35,8 +35,8 @@ constructor(props){
             <div className="details">
               <h2>What is Lorem Ipsum?</h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
               </p>
             </div>
@@ -49,8 +49,8 @@ constructor(props){
             <div className="details">
               <h2>What is Lorem Ipsum?</h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
               </p>
             </div>
@@ -63,57 +63,57 @@ constructor(props){
             <div className="details">
               <h2>What is Lorem Ipsum?</h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
           {this.state.showMore ? null:  <button class="more" onClick={()=>{this.setState({showMore: true})}} >Загрузить еще</button>}
-          {this.state.showMore ? <div className='videos__wrapper'> 
-          <div className="videos__item">
-            <div className="imgBx">
-              <img src={PUBLIC_URL + '/images/11.png'} alt="thumb" />
-            </div>
-
-            <div className="details">
-              <h2>What is Lorem Ipsum?</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div>
-          <div className="videos__item">
-            <div className="imgBx">
-              <img src={PUBLIC_URL + '/images/12.png'} alt="thumb" />
-            </div>
-
-            <div className="details">
-              <h2>What is Lorem Ipsum?</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div>
-          <div className="videos__item">
-            <div className="imgBx">
-              <img src={PUBLIC_URL + '/images/13.png'} alt="thumb" />
-            </div>
-
-            <div className="details">
-              <h2>What is Lorem Ipsum?</h2>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-          </div></div> : null}
         </div>
+        {this.state.showMore ? <div className="videos__wrapper">
+        <div className="videos__item">
+          <div className="imgBx">
+            <img src={PUBLIC_URL + '/images/11.png'} alt="thumb" />
+          </div>
+
+          <div className="details">
+            <h2>What is Lorem Ipsum?</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+        <div className="videos__item">
+          <div className="imgBx">
+            <img src={PUBLIC_URL + '/images/12.png'} alt="thumb" />
+          </div>
+
+          <div className="details">
+            <h2>What is Lorem Ipsum?</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+        <div className="videos__item">
+          <div className="imgBx">
+            <img src={PUBLIC_URL + '/images/13.png'} alt="thumb" />
+          </div>
+
+          <div className="details">
+            <h2>What is Lorem Ipsum?</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div></div> : null}
       </section>
     );
   }
