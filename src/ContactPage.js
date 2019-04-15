f3d04b4af41bd8a56bb048a4ebaf6f7e7e130cbb
Mariya <mariya.kapiturko@mail.ru>
2019-04-15 21:46:49 +0300

    Contact page -

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -7,13 +7,88 @@ const PUBLIC_URL = process.env.PUBLIC_URL;
 class ContactPage extends Component {
   render() {
     return (
-      <section className="contact-page">
-        <div class="flex-container">
-          <div class="flex-item one">Первый</div>
-          <div class="flex-item two">Второй</div>
-          <div class="flex-item three">Третий</div>
-        </div>
-      </section>
+      <div>
+        <section className="contact-page">
+          <div>
+            <h2>Features And Services</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique possimus inventore velit alias facilis asperiores
+              accusantium, fuga iusto repellat quia veritatis facere culpa
+              officiis consectetur dolorum, molestias magni distinctio iste?
+            </p>
+          </div>
+          <hr />
+
+          <ul className="grid">
+            <li>
+              <i className="fa fa-camera-retro" />
+              <h4>Photography</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                id felis et ipsum bibendum ultrices vitae pulvinar velit.
+              </p>
+            </li>
+            <li>
+              <i className="fa fa-cubes" />
+              <h4>Web Development</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                id felis et ipsum bibendum ultrices vitae pulvinar velit.
+              </p>
+            </li>
+            <li>
+              <i className="fa fa-newspaper-o" />
+              <h4>Content Editing</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                id felis et ipsum bibendum ultrices vitae pulvinar velit.
+              </p>
+            </li>
+          </ul>
+        </section>
+
+        <section className="reviews">
+          <h2 className="title">What others say:</h2>
+
+          <p className="quote">
+            Mauris sit amet mauris a arcu eleifend ultricies eget ut dolor.
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos.
+          </p>
+          <p className="author">— Patrick Farrell</p>
+
+          <p className="quote">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
+            felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed
+            aliquam dictum sapien, id sagittis augue malesuada eu.
+          </p>
+          <p className="author">— George Smith</p>
+
+          <p className="quote">
+            Donec commodo dolor augue, vitae faucibus tortor tincidunt in.
+            Aliquam vitae leo quis mi pulvinar ornare. Integer eu iaculis metus.
+          </p>
+          <p className="author">— Kevin Blake</p>
+        </section>
+
+        <section class="contact">
+          <h2 class="title">Join our newsletter</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
+            felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed
+            aliquam dictum sapien, id sagittis augue malesuada eu.
+          </p>
+          <hr />
+
+          <form>
+            <input type="email" placeholder="Email" />
+            <a href="/" class="btn">
+              Subscribe now
+            </a>
+          </form>
+        </section>
+      </div>
     );
   }
 }
--- src/ServicesPage.js
+++ src/ServicesPage.js
@@ -5,119 +5,138 @@ import './Services.css';
 const PUBLIC_URL = process.env.PUBLIC_URL;
 
 class ServicesPage extends Component {
-constructor(props){
-  super(props);
-  this.state= {
-    showMore: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMore: false
+    };
   }
-}
 
   render() {
     return (
       <section className="works">
-        <div><h2>How it works?</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-          possimus inventore velit alias facilis asperiores accusantium, fuga
-          iusto repellat quia veritatis facere culpa officiis consectetur
-          dolorum, molestias magni distinctio iste?
-        </p>
+        <div>
+          <h2>How it works?</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
+            possimus inventore velit alias facilis asperiores accusantium, fuga
+            iusto repellat quia veritatis facere culpa officiis consectetur
+            dolorum, molestias magni distinctio iste?
+          </p>
         </div>
         <hr />
-
 
         <div className="videos__wrapper">
           <div className="videos__item">
             <div className="imgBx">
               <img src={PUBLIC_URL + '/images/8.png'} alt="thumb" />
             </div>
-
             <div className="details">
               <h2>What is Lorem Ipsum?</h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
+
           <div className="videos__item">
             <div className="imgBx">
               <img src={PUBLIC_URL + '/images/9.png'} alt="thumb" />
             </div>
-
             <div className="details">
               <h2>What is Lorem Ipsum?</h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
+
           <div className="videos__item">
             <div className="imgBx">
               <img src={PUBLIC_URL + '/images/10.png'} alt="thumb" />
             </div>
-
             <div className="details">
               <h2>What is Lorem Ipsum?</h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
-          {this.state.showMore ? null:  <button class="more" onClick={()=>{this.setState({showMore: true})}} >Загрузить еще</button>}
         </div>
-        {this.state.showMore ? <div className="videos__wrapper">
-        <div className="videos__item">
-          <div className="imgBx">
-            <img src={PUBLIC_URL + '/images/11.png'} alt="thumb" />
-          </div>
+        {this.state.showMore ? null : (
+          <button
+            class="more"
+            onClick={() => {
+              this.setState({ showMore: true });
+            }}
+          >
+            Загрузить еще
+          </button>
+        )}
 
-          <div className="details">
-            <h2>What is Lorem Ipsum?</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-        <div className="videos__item">
-          <div className="imgBx">
-            <img src={PUBLIC_URL + '/images/12.png'} alt="thumb" />
-          </div>
+        {this.state.showMore ? (
+          <div className="videos__wrapper">
+            <div className="videos__item">
+              <div className="imgBx">
+                <img src={PUBLIC_URL + '/images/11.png'} alt="thumb" />
+              </div>
+              <div className="details">
+                <h2>What is Lorem Ipsum?</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </div>
 
-          <div className="details">
-            <h2>What is Lorem Ipsum?</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-        <div className="videos__item">
-          <div className="imgBx">
-            <img src={PUBLIC_URL + '/images/13.png'} alt="thumb" />
-          </div>
+            <div className="videos__item">
+              <div className="imgBx">
+                <img src={PUBLIC_URL + '/images/12.png'} alt="thumb" />
+              </div>
+              <div className="details">
+                <h2>What is Lorem Ipsum?</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </div>
 
-          <div className="details">
-            <h2>What is Lorem Ipsum?</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
+            <div className="videos__item">
+              <div className="imgBx">
+                <img src={PUBLIC_URL + '/images/13.png'} alt="thumb" />
+              </div>
+              <div className="details">
+                <h2>What is Lorem Ipsum?</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </div>
           </div>
-        </div></div> : null}
+        ) : null}
       </section>
     );
   }
 }
 
 export default ServicesPage;
-
